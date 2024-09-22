aa91959222111035005b004fb5b81d9410d31e36
nishinishic <81850624+nishinishic@users.noreply.github.com>
2024-09-23 04:00:31 +0800

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -14,19 +14,34 @@ function closeabouttoggleIframe() {
   }
 }
 
-function techtoggleIframe() {
-  const iframe = document.getElementById('techpopupFrame');
+function aitoggleIframe() {
+  const iframe = document.getElementById('aipopupFrame');
   // If display is empty (not yet set) or 'none', show the iframe
   if (iframe.style.display === 'none' || iframe.style.display === '') {
       iframe.style.display = 'block';
   }
 }
 
-function closetechtoggleIframe() {
-  const iframe = document.getElementById('techpopupFrame');
+function closeaitoggleIframe() {
+  const iframe = document.getElementById('aipopupFrame');
   // Close iframe when 'X' is clicked
   if (iframe.style.display === 'block') {
     iframe.style.display = 'none';
   }
 }
 
+function metadatatoggleIframe() {
+  const iframe = document.getElementById('metadatapopupFrame');
+  // If display is empty (not yet set) or 'none', show the iframe
+  if (iframe.style.display === 'none' || iframe.style.display === '') {
+      iframe.style.display = 'block';
+  }
+}
+
+function closemetadatatoggleIframe() {
+  const iframe = document.getElementById('metadatapopupFrame');
+  // Close iframe when 'X' is clicked
+  if (iframe.style.display === 'block') {
+    iframe.style.display = 'none';
+  }
+}
