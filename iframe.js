5c7815bf2197bed9531d4d1f8f9d0cfcd38f592a
nishinishic <81850624+nishinishic@users.noreply.github.com>
2024-09-22 21:51:03 +0800

    update and rename about.js to iframe.js & added close frame buttons

--- iframe.js
+++ iframe.js
@@ -13,3 +13,20 @@ function closeabouttoggleIframe() {
     iframe.style.display = 'none';
   }
 }
+
+function techtoggleIframe() {
+  const iframe = document.getElementById('techpopupFrame');
+  // If display is empty (not yet set) or 'none', show the iframe
+  if (iframe.style.display === 'none' || iframe.style.display === '') {
+      iframe.style.display = 'block';
+  }
+}
+
+function closetechtoggleIframe() {
+  const iframe = document.getElementById('techpopupFrame');
+  // Close iframe when 'X' is clicked
+  if (iframe.style.display === 'block') {
+    iframe.style.display = 'none';
+  }
+}
+
