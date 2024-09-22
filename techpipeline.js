0954e2fc09c97e3717742f41b42f4929dfe84f8b
nishinishic <81850624+nishinishic@users.noreply.github.com>
2024-09-22 21:40:04 +0800

    added close button

--- techpipeline.js
+++ techpipeline.js
@@ -1,13 +1,15 @@
 function techtoggleIframe() {
   const iframe = document.getElementById('techpopupFrame');
-  const button = event.target;  // Get the clicked button
-  
   // If display is empty (not yet set) or 'none', show the iframe
   if (iframe.style.display === 'none' || iframe.style.display === '') {
       iframe.style.display = 'block';
-      // button.textContent = 'Hide'; // Change button text
-  } else {
-      iframe.style.display = 'none';
-      // button.textContent = 'about'; // Change button text back
+  }
+}
+
+function closetechtoggleIframe() {
+  const iframe = document.getElementById('techpopupFrame');
+  // Close iframe when 'X' is clicked
+  if (iframe.style.display === 'block') {
+    iframe.style.display = 'none';
   }
 }
